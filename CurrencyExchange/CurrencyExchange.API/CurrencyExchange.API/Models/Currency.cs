f4dcc7009ecebc1e5deb47f54fca3d7e0966878b
Julia <juliawaloszek@gmail.com>
2019-09-28 13:45:02 +0200

    Create first models

---
+++ CurrencyExchange/CurrencyExchange.API/CurrencyExchange.API/Models/Currency.cs
@@ -0,0 +1,12 @@
+namespace CurrencyExchange.API.Models
+{
+    public class Currency
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public int Unit { get; set; }
+        public float PurchasePrice { get; set; }
+        public float SellPrice { get; set; }
+        public float AveragePrice { get; set; }
+    }
+}
