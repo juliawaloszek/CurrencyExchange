4c1f5983667ac56b6c31e22e4ac9ec2d77597bc6
Julia <juliawaloszek@gmail.com>
2019-09-28 18:09:05 +0200

    Update model, database create auth repository

--- CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Migrations/20190928155120_UpdateUser.Designer.cs
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Migrations/20190928155120_UpdateUser.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using CurrencyExchange.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CurrencyExchange.Api.Migrations
 {
     [DbContext(typeof(CurrencyExchangeContext))]
-    partial class CurrencyExchangeContextModelSnapshot : ModelSnapshot
+    [Migration("20190928155120_UpdateUser")]
+    partial class UpdateUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Repositories/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using CurrencyExchange.Api.Models;
+
+namespace CurrencyExchange.Api.Repositories
+{
+    public interface IAuthRepository
+    {
+        Task <User> Register(User user, string password);
+        Task <User> Login(string username, string password);
+        Task <bool> UserExists(string username);       
+    }
+}
