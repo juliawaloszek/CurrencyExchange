149bb67e6c5f3ab6c643427bf11f00eaa049999c
Julia <juliawaloszek@gmail.com>
2019-09-28 16:28:58 +0200

    Create user component in angular

--- CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/app.module.ts
+++ CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/app.module.ts
@@ -3,16 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      UserComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
---
+++ CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/user/user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+  users: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get('http://localhost:5003/api/users').subscribe(response => {
+      this.users = response;
+    }, error => {
+      console.log(error);
+    });
+  }
+}
