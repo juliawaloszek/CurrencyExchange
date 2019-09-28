15b0c1fd6e436bae10b64e36a9b81809d96c0770
Julia <juliawaloszek@gmail.com>
2019-09-28 16:42:29 +0200

    Display sth in angular

--- CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Startup.cs
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Startup.cs
@@ -29,6 +29,7 @@ namespace CurrencyExchange.Api
         {
             services.AddDbContext<CurrencyExchangeContext> (x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnecion")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +46,7 @@ namespace CurrencyExchange.Api
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
