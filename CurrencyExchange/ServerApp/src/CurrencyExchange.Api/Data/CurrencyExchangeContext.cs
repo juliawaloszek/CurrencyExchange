7e3c44105afaa38da84b788a5066173d28b0c9ed
Julia <juliawaloszek@gmail.com>
2019-09-28 14:52:37 +0200

    Create context and db connections

---
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Data/CurrencyExchangeContext.cs
@@ -0,0 +1,13 @@
+using CurrencyExchange.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CurrencyExchange.Api.Data
+{
+    public class CurrencyExchangeContext : DbContext
+    {
+        public CurrencyExchangeContext (DbContextOptions<CurrencyExchangeContext> options) : base (options) {}
+
+        
+        public DbSet<User> User {get; set; }       
+    }
+}
