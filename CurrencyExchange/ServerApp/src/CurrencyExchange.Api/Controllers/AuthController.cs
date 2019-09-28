a432805a064163735d632504cef27a62baaa41e3
Julia <juliawaloszek@gmail.com>
2019-09-28 19:04:16 +0200

    Add token authentication

--- CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Controllers/AuthController.cs
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Controllers/AuthController.cs
@@ -1,9 +1,14 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using CurrencyExchange.Api.Dtos;
 using CurrencyExchange.Api.Models;
 using CurrencyExchange.Api.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace CurrencyExchange.Api.Controllers
 {
@@ -37,6 +42,41 @@ namespace CurrencyExchange.Api.Controllers
 
             // return CreatedAtRoute()
             return StatusCode(201);
-        }      
+        } 
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.Username.ToLower(), userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new{
+                token = tokenHandler.WriteToken(token)
+            });
+        }     
     }
 }
