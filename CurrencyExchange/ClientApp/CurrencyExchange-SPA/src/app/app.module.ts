757587d2f476a75519e10f221df38215d6465a15
Julia <juliawaloszek@gmail.com>
2019-09-29 13:09:11 +0200

    Add angular material

--- CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/app.module.ts
+++ CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -19,7 +20,8 @@ import { NavComponent } from './nav/nav.component';
       NgbModule,
       BrowserModule,
       AppRoutingModule,
-      HttpClientModule
+      HttpClientModule,
+      BrowserAnimationsModule
    ],
    providers: [],
    bootstrap: [
