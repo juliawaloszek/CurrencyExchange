c9cabb97e4a5a8dc3305a4c50621e930725985e8
Julia <juliawaloszek@gmail.com>
2019-09-29 13:21:44 +0200

    test fashion plugins

--- CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Controllers/UsersController.cs
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyExchange.Api.Controllers
 {
-    [Authorize]
+    // [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class UsersController : ControllerBase
