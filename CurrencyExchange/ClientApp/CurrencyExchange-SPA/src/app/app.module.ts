d52c73b080c0bd2ac9266bf7aadae65b264b9478
Julia <juliawaloszek@gmail.com>
2019-09-29 13:46:01 +0200

    Work on register/login user

--- CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/app.module.ts
+++ CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { NavComponent } from './nav/nav.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AuthService } from './_services/auth.service';
+
+
 
 
 @NgModule({
@@ -21,9 +25,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      BrowserAnimationsModule
+      BrowserAnimationsModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
