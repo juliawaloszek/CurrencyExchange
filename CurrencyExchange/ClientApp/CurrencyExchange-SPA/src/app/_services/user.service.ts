c0dfae1b33ffd549e01feb9173680b2ee92b3bca
Julia <juliawaloszek@gmail.com>
2019-10-01 17:00:11 +0200

    work in progress

---
+++ CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/_services/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpHeaders } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    Authorization: 'Bearer ' + localStorage.getItem('token')
+  })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  baseUrl = environment.apiUrl;
+
+constructor() { }
+
+}
