f5dc2a68bee50cc5088d7853fdc2823e7129b0ee
Julia <juliawaloszek@gmail.com>
2019-10-01 17:36:44 +0200

    update wallet mockup

--- CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/main/my-wallet/my-wallet.component.ts
+++ CurrencyExchange/ClientApp/CurrencyExchange-SPA/src/app/main/my-wallet/my-wallet.component.ts
@@ -2,13 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Currency } from 'src/app/_models/currency';
 import { AuthService } from 'src/app/_services/auth.service';
 
-const DEFAULT_DATA: Currency[] = [
-  {name: 'US Dollar', code: 'USD', unit: 1, purchasePrice: 	3.7473, sellPrice: 	3.7563, averagePrice: 3.7518},
-  {name: 'Euro', code: 'EUR', unit: 1, purchasePrice: 4.2, sellPrice: 4.2447, averagePrice: 3.9942},
-  {name: 'Swiss Franc', code: 'CHF', unit: 1, purchasePrice: 3.2, sellPrice: 3.8913, averagePrice: 	3.8806},
-  {name: 'Russian ruble', code: 'RUB', unit: 100, purchasePrice: 7.2, sellPrice: 7.2652, averagePrice: 	7.5872},
-  {name: 'Czech koruna', code: 'CZK', unit: 100, purchasePrice: 15.2, sellPrice: 15.1595, averagePrice: 15.1309},
-  {name: 'Pound sterling', code: 'GBP', unit: 1, purchasePrice: 5.2, sellPrice: 5.6669, averagePrice: 5.6585}
+const DEFAULT_DATA: any[] = [
+  {name: 'US Dollar', code: 'USD', unit: 1, purchasePrice: 	3.7473, sellPrice: 	3.7563, averagePrice: 3.7518, amount: 100, value: 100},
+  {name: 'Euro', code: 'EUR', unit: 1, purchasePrice: 4.2, sellPrice: 4.2447, averagePrice: 3.9942, amount: 100, value: 100},
+  {name: 'Swiss Franc', code: 'CHF', unit: 1, purchasePrice: 3.2, sellPrice: 3.8913, averagePrice: 	3.8806, amount: 100, value: 100},
+  {name: 'Russian ruble', code: 'RUB', unit: 100, purchasePrice: 7.2, sellPrice: 7.2652, averagePrice: 	7.5872, amount: 100, value: 100},
+  {name: 'Czech koruna', code: 'CZK', unit: 100, purchasePrice: 15.2, sellPrice: 15.1595, averagePrice: 15.1309, amount: 100, value: 100},
+  {name: 'Pound sterling', code: 'GBP', unit: 1, purchasePrice: 5.2, sellPrice: 5.6669, averagePrice: 5.6585, amount: 100, value: 100}
 ];
 
 @Component({
@@ -18,7 +18,7 @@ const DEFAULT_DATA: Currency[] = [
 })
 export class MyWalletComponent implements OnInit {
 
-  displayedColumns: string[] = ['code', 'unit', 'purchasePrice', 'actions' ];
+  displayedColumns: string[] = ['code', 'unit', 'purchasePrice', 'amount', 'value', 'actions' ];
   dataSource = DEFAULT_DATA;
 
   constructor(private authService: AuthService) { }
