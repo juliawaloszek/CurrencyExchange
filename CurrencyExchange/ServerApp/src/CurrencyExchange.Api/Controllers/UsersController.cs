6bc068f9fd9553f53d0f8db153441e70c6bc6cfd
Julia <juliawaloszek@gmail.com>
2019-10-01 14:58:01 +0200

    generate guard

--- CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Controllers/UsersController.cs
+++ CurrencyExchange/ServerApp/src/CurrencyExchange.Api/Controllers/UsersController.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CurrencyExchange.Api.Controllers
 {
-    // [Authorize]
+    [Authorize]
     [AllowAnonymous]
     [Route("api/[controller]")]
     [ApiController]
